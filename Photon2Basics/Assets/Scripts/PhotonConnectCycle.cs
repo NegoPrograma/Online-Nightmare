917b14151ebfe85455ecbe4fb87f9ffd523d3d03
Isaac <isaacde2012@gmail.com>
2020-01-31 22:37:59 -0200

    Re-organized my folders, did some troubleshooting regarding connection issues and compatibility issues with Windows 7.

---
+++ Photon2Basics/Assets/Scripts/PhotonConnectCycle.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonConnectCycle : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnected(){
+        Debug.Log("onConnected stage achieved: next one should be ConnectedToMaster");
+    }
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("OnConnectedToMaster stage achieved, you're successfully connected to the server. you may enter the lobby now.");
+        Debug.Log("Server hosted at: "+ PhotonNetwork.CloudRegion + ", your ping on connection:" + PhotonNetwork.GetPing());
+    }
+
+
+}
